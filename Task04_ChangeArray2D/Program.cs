ce22f077ecd3e30ca5d4fbe04363b686460ec54b
NKompa <nkompa7@gmail.com>
2022-09-12 18:33:58 +0300

    Change elements of even indexes

---
+++ Task04_ChangeArray2D/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+int rows = int.Parse(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double [rows,columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(1,11);
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if(i%2==0 && j%2==0)
+        {
+            matrix[i,j] = Math.Pow(matrix[i,j],2);
+        }
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
