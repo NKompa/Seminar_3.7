ede9e517bf20eeb3e5789929fd9329416b486940
NKompa <nkompa7@gmail.com>
2022-09-12 18:30:02 +0300

    Fill 2D Array with random numbers

---
+++ Task03_FillArray2D/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.m = 3, n = 4.
+
+int rows = int.Parse(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [rows,columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(0,11);
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
