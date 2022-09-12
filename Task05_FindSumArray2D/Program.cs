c40c287836741fd2a93acbade4b1c95fdd8e15c6
NKompa <nkompa7@gmail.com>
2022-09-12 18:40:04 +0300

    Find sum of elements in 2D array

---
+++ Task05_FindSumArray2D/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+int rows = int.Parse(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [rows,columns];
+
+int count = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(1,11);
+        if(i==j) 
+        {
+            count = count + matrix[i,j];
+        }
+
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов диагонали = {count}");
